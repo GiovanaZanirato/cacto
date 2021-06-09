f197e2f9b786390b49e8db82394f9acd9b898ec2
GiovanaZanirato <84473403+GiovanaZanirato@users.noreply.github.com>
2021-06-09 16:57:03 -0300

    Add files via upload

---
+++ cacto.py
@@ -0,0 +1,12 @@
+for value in range(5):
+    print("Giovana")
+print("*****************************")
+number= list(range(2,9,2))
+print(number)
+print("****************************")
+square=[]
+print(square)
+for value in range(1,11):
+    squares=value*value
+    square.append("giovana")
+print(square)
